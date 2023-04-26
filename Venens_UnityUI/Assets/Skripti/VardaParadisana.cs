76b21d8278942ccd0832cfbc7059a41bcffec3cd
Patriks <patriks.venens@gmail.com>
2023-04-26 04:39:03 -0700

    izveidota poga un saliktas bildes

---
+++ Venens_UnityUI/Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class VardaParadisana : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+		public string teksts;
+
+		public GameObject ievadesLauks;
+
+		public GameObject tekstaAttelosana;
+
+		public void uzglabaTekstu(){
+			teksts = ievadesLauks.GetComponent<Text>().text;
+			tekstaAttelosana.GetComponent<Text>().text = "Sveiks "+teksts.ToUpper()+"!";
+
+
+
+		
+	}
+}
+
