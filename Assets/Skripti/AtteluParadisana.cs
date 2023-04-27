f99825438a2939e520cb06df3bdbe049ce560a92
Patriks <patriks.venens@gmail.com>
2023-04-27 14:42:24 +0300

    nomainitas bildes un uzrakstits toggle script

---
+++ Assets/Skripti/AtteluParadisana.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtteluParadisana : MonoBehaviour {
+	public GameObject Cilveks2;
+
+	public void SCilvekaAttelosana(bool vertiba){
+		Cilveks2.SetActive (vertiba);
+	}
+}
